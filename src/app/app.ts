5617dbcd523102eebe1a7c6556d87573687318f8
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-02-18 11:01:05 +0530

    feat: add Flowbite for enhanced UI components
    - Updated package.json to include Flowbite as a dependency.
    - Modified app.ts to initialize Flowbite on application startup.
    - Enhanced styles.css to support multiple themes from Flowbite, including default, enterprise, playful, and mono themes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
   protected readonly title = signal('ape-kade');
 }
