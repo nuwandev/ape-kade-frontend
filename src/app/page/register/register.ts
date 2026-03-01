a9ff379454c3ccd5e1da640cd8d532f132c27ebd
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-03-02 00:24:56 +0530

    feat: implement reactive form for registration with validation and error messages

--- src/app/page/register/register.ts
+++ src/app/page/register/register.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  private readonly fb = inject(FormBuilder);
+  registerForm = this.fb.group({
+    businessName: ['', [Validators.required, Validators.minLength(3)]],
+    username: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z0-9_]+$/)],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
 
+  onRegister() {}
 }
