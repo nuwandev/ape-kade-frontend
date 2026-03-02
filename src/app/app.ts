43702a5d9f30dab5154d40b0996bda0f3929236b
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-03-02 11:46:44 +0530

    refactor: remove flowbite dependency and initialization from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { initFlowbite } from 'flowbite';
-import { Toast } from "./component/toast/toast";
+import { Toast } from './component/toast/toast';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +8,6 @@ import { Toast } from "./component/toast/toast";
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  ngOnInit(): void {
-    initFlowbite();
-  }
+export class App {
   protected readonly title = signal('ape-kade');
 }
